2b8e127764347d12504e3f069b633d71ce641d6d
ziadseleem <ziad32seleem@gmail.com>
2026-03-14 07:30:48 +0200

    fix for the pizza proj. removal for the tailwind and added ul for the menu.jsx

--- 07 - pizza menu/src/components/Menu.jsx
+++ 07 - pizza menu/src/components/Menu.jsx
@@ -6,9 +6,11 @@ function Menu() {
   return (
     <main className='menu' >
       <h2>Our Menu</h2>
-      {pizzaData.map((pizza) => (
-        <Pizza key={pizza.name} name={pizza.name} ingredients={pizza.ingredients} price={pizza.price} photoName={pizza.photoName} />
-      ))}
+      <ul className='pizzas' >
+        {pizzaData.map((pizza) => (
+          <Pizza key={pizza.name} name={pizza.name} ingredients={pizza.ingredients} price={pizza.price} photoName={pizza.photoName} />
+        ))}
+      </ul>
     </main>
   )
 }
