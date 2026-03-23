6e71594a22ae36189242f7225593a50f9519405b
ziadseleem <ziad32seleem@gmail.com>
2026-03-24 00:14:46 +0200

    add "sort by" basic state in the list

--- 10 - Far Away_travel list/src/components/PackingList.jsx
+++ 10 - Far Away_travel list/src/components/PackingList.jsx
@@ -1,11 +1,23 @@
 import Item from "./Item";
+import { useState } from 'react'
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+    const [sortby, setSortby] = useState('input')
+
     return (
         <div className="list">
             <ul>
                 {items.map(item => (<Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />))}
             </ul>
+
+            <div className="actions">
+                <select value={sortby} onChange={(e) => setSortby(e.target.value)}>
+                    <option value="input">Sort by input order</option>
+                    <option value="description">Sort by description</option>
+                    <option value="packed">Sort by packed status</option>
+                </select>
+            </div>
+
         </div>
     )
 }
