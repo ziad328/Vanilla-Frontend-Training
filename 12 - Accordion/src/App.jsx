5bf3c2b9375b8cff181fb5dddb63771cf5ca91b6
ziadseleem <ziad32seleem@gmail.com>
2026-03-25 01:52:59 +0200

    finsihed accordion component and accordionItem

--- 12 - Accordion/src/App.jsx
+++ 12 - Accordion/src/App.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import Accordion from "./Accordion";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text:
+      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text:
+      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
+  },
+  {
+    title: "Do you provide technical support?",
+    text:
+      "Nisi, voluptate. Doloribus, voluptate. Doloribus, voluptate. Doloribus, voluptate. Doloribus, voluptate. Doloribus, voluptate."
+  },
+  {
+    title: "What is your return policy?",
+    text:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+  }
+];
+
 
 function App() {
   return (
-    <div>App</div>
+    <Accordion data={faqs} />
   )
 }
 
