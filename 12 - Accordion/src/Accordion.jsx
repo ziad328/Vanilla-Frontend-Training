3723b3952c8e442a48fd779b3f77cae5d8c97a51
ziadseleem <ziad32seleem@gmail.com>
2026-03-25 02:00:31 +0200

    made the accordion item generic

--- 12 - Accordion/src/Accordion.jsx
+++ 12 - Accordion/src/Accordion.jsx
@@ -1,13 +1,39 @@
 import AccordionItem from "./AccordionItem"
+import { useState } from "react";
 
 function Accordion({ data }) {
+    const [curOpen, setCurOpen] = useState(null);
+
     return (
         <div className="accordion">
-            {data.map((item, index) => (
-                <AccordionItem key={index} num={index} title={item.title} text={item.text} />
+            {data.map((el, i) => (
+                <AccordionItem
+                    curOpen={curOpen}
+                    onOpen={setCurOpen}
+                    title={el.title}
+                    num={i}
+                    key={el.title}
+                >
+                    {el.text}
+                </AccordionItem>
             ))}
+
+            <AccordionItem
+                curOpen={curOpen}
+                onOpen={setCurOpen}
+                title="Test 1"
+                num={5}
+                key="test 1"
+            >
+                <p>Allows React developers to:</p>
+                <ul>
+                    <li>Break up UI into components</li>
+                    <li>Make components reusuable</li>
+                    <li>Place state efficiently</li>
+                </ul>
+            </AccordionItem>
         </div>
-    )
+    );
 }
 
 export default Accordion
--- 12 - Accordion/src/AccordionItem.jsx
+++ 12 - Accordion/src/AccordionItem.jsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
-
-function AccordionItem({ num, title, text }) {
-    const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+    const isOpen = num === curOpen;
 
     function handleToggle() {
-        setIsOpen((isOpen) => !isOpen);
+        onOpen(isOpen ? null : num);
     }
 
     return (
@@ -13,9 +11,9 @@ function AccordionItem({ num, title, text }) {
             <p className="title">{title}</p>
             <p className="icon">{isOpen ? "-" : "+"}</p>
 
-            {isOpen && <div className="content-box">{text}</div>}
+            {isOpen && <div className="content-box">{children}</div>}
         </div>
-    )
+    );
 }
 
 export default AccordionItem
